8329d5ec611660e46c244fdc478b02bfc1e37045
pyyple <112457126+pyyple@users.noreply.github.com>
2022-08-30 04:56:23 +0100

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,1 +1,63 @@
+<!DOCTYPE html>
+<html lang="ru">
 
+<head>
+	<meta charset="utf-8">
+	<title>WebStore</title>
+	<link rel="stylesheet" href="css/develop-outlines.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="img/title.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="img/title.ico" type="image/x-icon">
+</head>
+
+<body>
+
+	<header class="page-header">
+		<div class="top-container">
+			<a class="logo">
+				<img src="img/logo.png" alt="Логотип компании WebStore" width="150" height="150">
+				WebStore
+			</a>
+			<nav>
+				<ul class="head-nav-list">
+					<li class="head-nav-item"><a href="#about">О компании</a></li>
+				</ul>
+			</nav>
+			<a href="tel:+79818574624" class="phone">+7 (111) 111-11-11</a>
+		</div>
+	</header>
+
+	<main>
+		<section class="intro">
+			<div class="container">
+				<div class="intro-text">
+					<h1 class="intro-header">Главный заголовок сайта</h1>
+					<p class="intro-description">Краткое описание магазина
+					</p>
+					<a href="#request-form" class="button request-button">Оставить заявку</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="about" id="about">
+
+			<h2 class="section-header">
+				О нас
+			</h2>
+
+			<img src="img/about.jpg" alt="Описание фото" class="about-image">
+
+			<div class="about-description">
+				<p>Что-нибудь о продавце и магазине.</p>
+			</div>
+
+		</section>
+	</main>
+
+	<footer class="page-footer">
+		Подвал сайта
+	</footer>
+
+</body>
+
+</html>
